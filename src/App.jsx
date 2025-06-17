86a5e6314f77cfbcb6cfb27992b9f484e450c0b4
Prakhar Bhawsar <jmbliss46@gmail.com>
2025-06-17 13:11:43 +0530

    some update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/flavours" element={<Flavours />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+    <div className='container'>
+    <div className='wrapper'>
+      <div className='logo'><img src='/public/20250617_1104_Vision Frame Logo_simple_compose_01jxy61tznf1mssmg4c0bxckma.png'></img></div>
+      <ul className='Menu-wrapper'>
+        <li>About</li>
+         <li>Service</li>
+          <li>Contact</li>
+      </ul>
+    </div>
+
+    </div>
+    </div>
+  )
+}
+
+export default Header
