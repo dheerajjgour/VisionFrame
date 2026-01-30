6931fff2b98db9c049c7f4f4c42cd55b32b4a8bb
Rupesh Tripathi <jmbliss92@gmail.com>
2026-01-30 17:45:08 +0530

    push after responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,75 +1,86 @@
-import React from "react";
+import React from 'react'
 import { FaInstagram, FaLinkedinIn, FaFacebookF, FaXTwitter, FaSkype } from "react-icons/fa6";
-
 const Footer = () => {
   return (
-    <footer>
-      <div className="container">
-        <div className="row-1">
-          <div className="col-1">
-            <h5>Let’s make <br/>
-            something <br/>amazing together.</h5>
-          </div>
-          <div className="col-2">
-            <h5>QUICK LINKS</h5>
-             <ul>
-             <li><a href='#Home'>Home</a></li>
-            <li><a href='#Service'>Service</a></li>
-             <li ><a href='#Project'>Project</a></li>
-            <li ><a href='#Contact'>Contact</a></li>
-          </ul>
-          </div>
-          <div className="col-3">
-            <div className="add-wrapper">
-            <h5>Address</h5>
-            <p>1301, Skye Corporate Park, Plot No. 25, Scheme No. 78-II, AB Road, Indore, India.</p>
-            </div>
-              <div className="phone-wrapper">
-            <h5>Phone</h5>
-            <p>9171399990</p>
-            </div>
-              <div className="phone-wrapper">
-            <h5>E-mail</h5>
-            <p>hello@ashvinsharma.com</p>
-            </div>
-          </div>
+  <footer>
+  <div className="container">
+    <div className="row-1">
+      <div className="col-1">
+        <h5>
+          Let’s make <br />
+          something <br />amazing together.
+        </h5>
+      </div>
+
+      <div className="col-2">
+        <h5>QUICK LINKS</h5>
+        <ul>
+          <li><a href="#Home">Home</a></li>
+          <li><a href="#Service">Service</a></li>
+          <li><a href="#Project">Project</a></li>
+          <li><a href="#Contact">Contact</a></li>
+        </ul>
+      </div>
+
+      <div className="col-3">
+        <div className="add-wrapper">
+          <h5>Address</h5>
+          <p>
+            1301, Skye Corporate Park, Plot No. 25, Scheme No. 78-II,
+            AB Road, Indore, India.
+          </p>
         </div>
-        <div className="row-2">
-          <div className="col-1">
-            <p>Start by saying hi</p>
-          </div>
-<div className="col-2">
-  <div className="social-wrapper">
-    <a href="https://www.instagram.com/ashvin0731/" className="social-icon" target="_blank" rel="noreferrer">
-      <FaInstagram />
-    </a>
 
-    <a href="https://linkedin.com" className="social-icon" target="_blank" rel="noreferrer">
-      <FaLinkedinIn />
-    </a>
-
-    <a href="https://www.facebook.com/ashvin.sharma.31" className="social-icon" target="_blank" rel="noreferrer">
-      <FaFacebookF />
-    </a>
-
-    <a href="https://twitter.com" className="social-icon" target="_blank" rel="noreferrer">
-      <FaXTwitter />
-    </a>
-
-
-  </div>
-</div>
-
-
+        <div className="phone-wrapper">
+          <h5>Phone</h5>
+          <p>
+            <a href="tel:+919171399990">+91 91713 99990</a>
+          </p>
         </div>
-        <div className="row-3">
-          <div className="col-1">
-            <p>Copyright © 2026 Ashvin Sharma ,All Rights Reserved</p>
-          </div>
+
+        <div className="phone-wrapper">
+          <h5>E-mail</h5>
+          <p>
+            <a href="mailto:hello@ashvinsharma.com">
+              hello@ashvinsharma.com
+            </a>
+          </p>
         </div>
       </div>
-    </footer>
-  );
-};
+    </div>
 
-export default Footer;
+    <div className="row-2">
+      <div className="col-1">
+        <p>Start by saying hi</p>
+      </div>
+
+      <div className="col-2">
+        <div className="social-wrapper">
+          <a href="https://www.instagram.com/ashvin0731/" className="social-icon" target="_blank" rel="noreferrer">
+            <FaInstagram />
+          </a>
+          <a href="https://linkedin.com" className="social-icon" target="_blank" rel="noreferrer">
+            <FaLinkedinIn />
+          </a>
+          <a href="https://www.facebook.com/ashvin.sharma.31" className="social-icon" target="_blank" rel="noreferrer">
+            <FaFacebookF />
+          </a>
+          <a href="https://twitter.com" className="social-icon" target="_blank" rel="noreferrer">
+            <FaXTwitter />
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div className="row-3">
+      <div className="col-1">
+        <p>Copyright © 2026 Ashvin Sharma. All Rights Reserved.</p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+  )
+}
+
+export default Footer
--- src/components/RecentWork.jsx
+++ src/components/RecentWork.jsx
@@ -1,29 +1,66 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function RecentWork() {
-    const project = [
-    { title: 'Velnik', image: 'projects/velnik-homepage.png', url:'https://velnik.com/'},
-    { title: 'Babelois', image: 'projects/babelois-homepage.webp', url:'https://babelois.in/' },
-     { title: 'Kaveri Mehendi', image: 'projects/Kaveri.png', url:'https://kaverimehendi.in/' },
-    { title: 'Elois', image: 'projects/elois-homepage.png',url:'https://elois.in/' },
-    { title: 'Nutrigain', image: 'projects/Nutrigain.png',url:'http://nutrigain.in/' },
+  const project = [
+    { title: "Velnik", image: "projects/velnik-homepage.png", url: "https://velnik.com/" },
+    { title: "Babelois", image: "projects/babelois-homepage.webp", url: "https://babelois.in/" },
+    { title: "Kaveri Mehendi", image: "projects/Kaveri.png", url: "https://kaverimehendi.in/" },
+    { title: "Elois", image: "projects/elois-homepage.png", url: "https://elois.in/" },
+    { title: "Nutrigain", image: "projects/Nutrigain.png", url: "http://nutrigain.in/" },
   ];
-  return (
-    <section className="recent-work" id='Project'>
-  <h2 style={{ textAlign: "center", marginBottom: "20px" }}>Recent Work</h2>
-  <div className="row-wrapper container">
-    {project.map((project, idx) => (
-      <div className={`row-${idx}`} key={idx}>
-        <a href={project.url} target="_blank" rel="noopener noreferrer">
-          <img src={project.image} alt={project.title} />
-        </a>
-        <div className="title">{project.title}</div>
-      </div>
-    ))}
-  </div>
-</section>
+useEffect(() => {
+  gsap.utils.toArray(".project-card").forEach((card, index) => {
+    gsap.fromTo(
+      card,
+      {
+        opacity: 0,
+        x: index % 2 === 0 ? -120 : 120,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        ease: "power3.out", 
+        scrollTrigger: {
+          trigger: card,
+          start: "top 80%",
+        },
+      }
+    );
+  });
+}, []);
 
-  )
+
+  return (
+    <section className="recent-work" id="Project">
+      <h2 className="section-title">Recent Work</h2>
+
+      <div className="row-wrapper container">
+        {project.map((item, idx) => (
+          <a
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`project-card ${idx % 2 === 0 ? "left" : "right"}`}
+            key={idx}
+          >
+            <div className="image-wrap">
+              <img src={item.image} alt={item.title} />
+            </div>
+
+            <div className="project-info">
+              <h3>{item.title}</h3>
+              <span className="view-project">View Project →</span>
+            </div>
+          </a>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default RecentWork
+export default RecentWork;
